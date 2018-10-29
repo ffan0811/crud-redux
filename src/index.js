3a462a58ad71ca71b38f3c18189960b1b117b8ce
mijeong <ffan0811@naver.com>
2018-10-29 11:33:47 +0900

    redux-persist 적용 중

--- src/index.js
+++ src/index.js
@@ -4,26 +4,29 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage/session";
-
+import storage from "redux-persist/lib/storage";
 import rootReducer from './reducers/postReducer';
 
 const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["counter"]
+	key: 'root',
+	storage,
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const pReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(persistedReducer);
-const persistor = persistStore(store);
+
+const store = createStore(pReducer);
+const persistor = persistStore(store)
+
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<PersistGate loading={<div>loading</div>} persistor={persistor}>
+			<App />
+		</PersistGate>
 	</Provider>
 	, document.getElementById('root')
 );
