416fc051f59dae4fa4a27251b94ab1a7b2ceb9ee
mijeong <ffan0811@naver.com>
2018-10-29 10:05:15 +0900

    .

--- src/Post.js
+++ src/Post.js
@@ -6,10 +6,10 @@ class Post extends Component {
 	render(){
 		return(
 			<div>
-				<h2>{this.props.post.title}</h2>
-				<p>{this.props.post.message}</p>
-				<button onClick={() => this.props.dispatch({type:'EDIT_POST', id:this.props.post.id})}>Edit</button>
-				<button onClick={() => this.props.dispatch({type:'DELETE_POST', id:this.props.post.id})}>Delete</button>
+				<h2>{this.props.postt.title}</h2>
+				<p>{this.props.postt.message}</p>
+				
+				<button onClick={() => this.props.dispatch({type:'DELETE_POST', id:this.props.postt.id})}>Delete</button>
 			</div>
 		)
 	}
